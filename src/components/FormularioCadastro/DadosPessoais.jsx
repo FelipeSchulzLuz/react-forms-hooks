6994b69c90c2746f671a6b9355e74952507cdd3f
Felipe Schulz <36828158+FelipeSchulzLuz@users.noreply.github.com>
2021-06-22 20:19:23 -0300

    validacao de campos

--- src/components/FormularioCadastro/DadosPessoais.jsx
+++ src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,26 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import ValidacoesCadastro from "../../context/ValidacoesCadastro"
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
-function DadosPessoais({ aoEnviar, validarCPF }) {
+function DadosPessoais({ aoEnviar }) {
   const [nome, setNome] = useState("");
-  const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
   const [promocoes, setPromocoes] = useState(true);
   const [novidades, setNovidades] = useState(true);
-  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" } });
+  const [erros, setErros] = useState({ cpf: { valido: true, texto: "" }, nome: { valido: true, texto: "" } });
+
+  const validacoes = useContext(ValidacoesCadastro)
+  
+  function validarCampos(e) {
+    const {name, value} = e.target;
+    const novoEstado = {...erros};
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+  }
+
+  function possoEnviar(){
+    for(let campo in erros) {
+      if(!erros[campo].valido) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
+        if(possoEnviar()){
+          aoEnviar({ nome, sobrenome, cpf, promocoes, novidades });
+        }
       }}
     >
       <TextField
         value={nome}
-        onChange={(e) => {
-          setNome(e.target.value);
-        }}
+        onBlur={validarCampos}
+        onChange={(e) => { setNome(e.target.value);}}
+        name="nome"
+        error={!erros.nome.valido}
+        helperText={erros.nome.texto}
         margin="normal"
         id="nome"
         label="Nome"
@@ -30,9 +53,8 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
       />
       <TextField
         value={sobrenome}
-        onChange={(e) => {
-          setSobrenome(e.target.value);
-        }}
+        onChange={(e) => { setSobrenome(e.target.value); }}
+        name="sobrenome"
         margin="normal"
         id="sobrenome"
         label="Sobrenome"
@@ -45,10 +67,8 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
         onChange={(e) => {
           setCpf(e.target.value);
         }}
-        onBlur={(e) => {
-          const isValid = validarCPF(cpf);
-          setErros({ cpf: isValid });
-        }}
+        onBlur={validarCampos}
+        name="cpf"
         error={!erros.cpf.valido}
         helperText={erros.cpf.texto}
         margin="normal"
@@ -65,7 +85,7 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
             checked={promocoes}
             onChange={(event) => setPromocoes(event.target.checked)}
             variant="contained"
-            name="Promoções"
+            name="promocoes"
             color="primary"
             type="checkbox"
           />
@@ -78,14 +98,14 @@ function DadosPessoais({ aoEnviar, validarCPF }) {
             checked={novidades}
             onChange={(event) => setNovidades(event.target.checked)}
             variant="contained"
-            name="Novidades"
+            name="novidades"
             color="primary"
             type="checkbox"
           />
         }
       />
       <Button size="large" variant="contained" color="primary" type="submit">
-        Cadastrar
+        Próximo
       </Button>
     </form>
   );
--- src/components/FormularioCadastro/DadosUsuarios.jsx
+++ src/components/FormularioCadastro/DadosUsuarios.jsx
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, TextField } from "@material-ui/core";
+import ValidacoesCadastro from "../../context/ValidacoesCadastro";
 
 function DadosUsuario({ aoEnviar }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+
+  const [erros, setErros] = useState({ senha: { valido: true, texto: "" } });
+
+  const validacoes = useContext(ValidacoesCadastro)
+
+  function validarCampos(e) {
+    const {name, value} = e.target;
+    const novoEstado = {...erros};
+    novoEstado[name] = validacoes[name](value);
+    setErros(novoEstado);
+  }
+
+  function possoEnviar() {
+    for(let campo in erros) {
+      if(!erros[campo].valido) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        aoEnviar({email, senha});
+        if(possoEnviar()){
+          aoEnviar({ email, senha });
+        }
       }}
     >
       <TextField
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         id="email"
+        name="email"
         label="email"
         type="email"
         variant="outlined"
@@ -24,9 +49,15 @@ function DadosUsuario({ aoEnviar }) {
       />
       <TextField
         value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-        id="password"
-        label="password"
+        onChange={(e) => {
+          setSenha(e.target.value);
+        }}
+        onBlur={validarCampos}
+        error={!erros.senha.valido}
+        helperText={erros.senha.texto}
+        name="senha"
+        id="senha"
+        label="senha"
         type="password"
         variant="outlined"
         margin="normal"
@@ -34,7 +65,7 @@ function DadosUsuario({ aoEnviar }) {
         fullWidth
       />
       <Button size="large" variant="contained" color="primary" type="submit">
-        Cadastrar
+        Próximo
       </Button>
     </form>
   );
